6acb0dfe82ac33ec74239030510710666851cd7f
calexgm.dev <calexgm.dev@gmail.com>
2024-05-08 21:52:55 -0500

    second commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import jsconfigPaths from 'vite-jsconfig-paths';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), jsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: /^~(.+)/,
+        replacement: path.join(process.cwd(), 'node_modules/$1')
+      },
+      {
+        find: /^src(.+)/,
+        replacement: path.join(process.cwd(), 'src/$1')
+      }
+    ]
+  },
+})
